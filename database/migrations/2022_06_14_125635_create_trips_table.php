c5bb6579d6cc867776a5a62ddaa49c87bbac9d3a
MirkOrlando <mirko@orlando-m.it>
2022-06-14 15:27:00 +0200

    create migration, seeder, model

---
+++ database/migrations/2022_06_14_125635_create_trips_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('location', 50);
+            $table->decimal('price', 6, 2)->unsigned();
+            $table->tinyInteger('duration(days)')->unsigned()->nullable();
+            $table->tinyInteger('duration(nights)')->unsigned()->nullable();
+            $table->string('accomodation', 15)->nullable();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
